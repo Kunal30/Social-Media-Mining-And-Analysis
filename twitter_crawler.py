dace128f66999d57fec134f5339990b18d3d1252
Kunal30 <kunalvsuthar@gmail.com>
2019-09-17 16:42:02 -0700

    Stored all vertices data

--- twitter_crawler.py
+++ twitter_crawler.py
@@ -2,6 +2,7 @@
 import oauth2
 from python_config_reader import read_config
 import twitter
+import pickle
 
 def main():
 	config=read_config(filename='/media/kunal/Windows8_OS/ASU Fall 19/CSE-472 Social Media Mining/configs/config.twitter.ini',section='twitter')
@@ -11,44 +12,89 @@ def main():
                   access_token_key=config['token_key'],
                   access_token_secret=config['token_secret'])
 
+	f=open("v_names.pickle","rb")
+	v_names=pickle.load(f)
+	f=open("v_ids.pickle","rb")
+	v_ids=pickle.load(f)	
+	
+	# Removing duplicates
+	v_names=list(set(v_names))	
+	v_ids=list(set(v_ids))	
+	
+	edges=create_edges(v_names=v_names,v_ids=v_ids,api=api)
+
+def create_edges(v_names,v_ids,api):
+	"""
+	Create edges between v_ids
+	"""
+	for v in v_ids:
+		following_list=api.GetFriendIDs(user_id=v)
+		print(following_list)
+		print('############################################')
+
+def get_all_data(api):
+	"""
+	Uses twitter api to get following data 
+	of cricketcomau, ECB Cricket, BCCI, ESPN_Cricinfo,absycric
+	and dumps the data in a pickle file
+
+	Returns Union of all followers
+	"""
+
 	user_1=api.GetFriendIDs(screen_name='cricketcomau')
-	print(len(user_1))
-	print('############################################')
+	f = open("user_1.pickle", "wb")
+	pickle.dump(user_1,f)
+
 	user_2=api.GetFriendIDs(screen_name='ECB_cricket')
-	print(len(user_2))
-	print('############################################')
+	f = open("user_2.pickle", "wb")
+	pickle.dump(user_2,f)
+
 	user_3=api.GetFriendIDs(screen_name='BCCI')
-	print(len(user_3))
-	print('############################################')
+	f = open("user_3.pickle", "wb")
+	pickle.dump(user_3,f)
+
 	user_4=api.GetFriendIDs(screen_name='ESPNcricinfo')
-	print(len(user_4))
+	f = open("user_4.pickle", "wb")
+	pickle.dump(user_4,f)
+
 	user_5=api.GetFriendIDs(screen_name='absycric')
-	print(len(user_5))
-	union=list(set().union(user_1,user_2,user_3,user_4,user_5))
-	print('############################################')
-	print(len(union))
-	print(union)
-	v_names,v_ids=getNames(id_set=union,api=api)
-	print(v_names)
-	print(v_ids)
-	print(len(v_names))
-	print(len(v_ids))
+	f = open("user_5.pickle", "wb")
+	pickle.dump(user_5,f)
+
+	user_6=api.GetFriendIDs(screen_name='cricbuzz')
+	f = open("user_6.pickle", "wb")
+	pickle.dump(user_6,f)
+
+	union=list(set().union(user_1,user_2,user_3,user_4,user_5,user_6))
+	return union
 
 def getNames(api,id_set=[]):
 	"""
 	Returns two lists of verified Names 
 	& user ids
 	"""	
-	verified_names=[]
-	verified_ids=[]
-	for i in id_set:
-		print('Inside the loop')
-		print(i)
-		user=api.GetUser(user_id=i)
-		if user.verified:
-			verified_names.append(user.name)
-			verified_ids.append(i)
+	f=open('v_names.pickle','rb')
+	verified_names=pickle.load(f)
+	f=open('v_ids.pickle','rb')
+	verified_ids=pickle.load(f)
+
+	try:
+		for i in id_set:
+			print('Inside the loop')
+			print(i)
+			user=api.GetUser(user_id=i)
+			if user.verified:
+				print(user.name)
+				verified_names.append(user.name)
+				verified_ids.append(i)
 	
+	except Exception as	error:
+		print(error)
+		f=open("verified_names.pickle","wb")
+		pickle.dump(verified_names,f)
+		f=open("verified_ids.pickle","wb")
+		pickle.dump(verified_ids,f)		
+
 	return verified_names,verified_ids		
 
 
